0fafbe6f62ab8f3fe80a4300b99c1a59dc5f151e
Malengo <jose.malengo@hotmail.com>
2022-02-25 18:00:04 -0300

    Add new paramther

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -6,29 +6,31 @@ namespace ApiStoreWeb.Models
 {
     public class Smartphone
     {
-       
 
-        public Smartphone(int id, string? brand, string? modelDescription, string? color, string? capacity, double imeiOne, double imeiTwo)
+
+        public Smartphone(int id, string? brand, string? modelDescription, string? color, string? capacity, string memory, double imeiOne, double imeiTwo)
         {
             Id = id;
             Brand = brand;
             this.modelDescription = modelDescription;
             Color = color;
             Capacity = capacity;
+            Memory = memory;
             ImeiOne = imeiOne;
             ImeiTwo = imeiTwo;
         }
-       
+
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set;}
+        public int Id { get; set; }
         public string? Brand { get; set; }
         public string? modelDescription { get; set; }
         public string? Color { get; set; }
         public string? Capacity { get; set; }
+        public string? Memory { get; set; }
         public double ImeiOne { get; set; }
         public double ImeiTwo { get; set; }
 
-        
+
     }
 
 }

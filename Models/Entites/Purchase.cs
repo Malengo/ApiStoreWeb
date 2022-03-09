d9fd2ff82fd68585fe5077709721a7093a444eff
Malengo <jose.malengo@hotmail.com>
2022-03-09 09:50:11 -0300

    create entite Purchase

---
+++ Models/Entites/Purchase.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiStoreWeb.Models.Entites
+{
+    public class Purchase
+
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("smartphoneId")]
+        public virtual Smartphone smartphoneId { get; set; }
+        public DateTime dayPurchase { get; set; }
+        public double valuePurchase { get; set; }
+
+    }
+}

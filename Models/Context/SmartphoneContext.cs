67462bdbe34312503e235953e96003e8556acf07
Malengo <jose.malengo@hotmail.com>
2022-03-12 16:12:01 -0300

    fixing bug

--- Models/Context/SmartphoneContext.cs
+++ Models/Context/SmartphoneContext.cs
@@ -1,3 +1,4 @@
+using ApiStoreWeb.Models.Entites;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -12,9 +13,13 @@ namespace ApiStoreWeb.Models.Context
         public DbSet<Smartphone> smartphones { get; set; }
         public DbSet<Purchase> purchases { get; set; }
 
+        public DbSet<User> user { get; set;}
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Smartphone>().ToTable("smartphone");
+
+            modelBuilder.Entity<Smartphone>().ToTable("smartphones");
+            modelBuilder.Entity<User>().ToTable("usuarios");
             modelBuilder.Entity<Purchase>().ToTable("purchase");
         }
     }

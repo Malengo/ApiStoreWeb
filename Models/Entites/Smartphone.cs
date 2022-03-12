d2103d136b7e7f8031aa7e33723841799c0e385b
Malengo <jose.malengo@hotmail.com>
2022-03-12 16:16:25 -0300

    fixing bug

--- Models/Entites/Smartphone.cs
+++ Models/Entites/Smartphone.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 
-namespace ApiStoreWeb.Models
+namespace ApiStoreWeb.Models.Entites
 {
     public class Smartphone
     {
